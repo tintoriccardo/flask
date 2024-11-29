9f8afc6f7698907d425aa729b863f80788eb54b4
riccardo tinto <tinto.riccardo@iisgalvanimi.edu.it>
2024-11-29 12:45:58 +0000

    ciao

--- file.py
+++ file.py
@@ -1,11 +1,28 @@
-from flask import Flask,  render_template
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+lista_spesa = []
+
 @app.route('/')
 def home():
-    return render_template('home.html')
-    
+    return render_template('home.html', lista=lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    elemento = request.form['elemento']
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
